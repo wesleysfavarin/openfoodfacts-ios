2c8689f7898957f598ccb3baa2abc0be2f73dc25
Rudrank Riyam <rudrankriyam@gmail.com>
2019-03-02 16:35:16 +0530

    remove the function for contact alert

--- Sources/ViewControllers/Settings/SettingsTableViewController.swift
+++ Sources/ViewControllers/Settings/SettingsTableViewController.swift
@@ -98,9 +98,7 @@ class SettingsTableViewController: UITableViewController, MFMailComposeViewContr
         let mailComposeViewController = configuredMailComposeViewController()
         if MFMailComposeViewController.canSendMail() {
             self.present(mailComposeViewController, animated: true, completion: nil)
-        } else {
-            self.showSendMailErrorAlert()
-        }
+       }
     }
     func configuredMailComposeViewController() -> MFMailComposeViewController {
         let mailComposerViewController = MFMailComposeViewController()
@@ -110,12 +108,7 @@ class SettingsTableViewController: UITableViewController, MFMailComposeViewContr
         mailComposerViewController.setMessageBody("settings.contact.message".localized, isHTML: false)
         return mailComposerViewController
     }
-    func showSendMailErrorAlert() {
-        let sendMailErrorAlert = UIAlertController(title: "settings.contact-alert.title".localized, message: "settings.contact-alert.message".localized, preferredStyle: .alert)
-        let yesAction = UIAlertAction(title: "alert.action.ok".localized, style: .default)
-        sendMailErrorAlert.addAction(yesAction)
-        self.present(sendMailErrorAlert, animated: true, completion: nil)
-    }
+
     func mailComposeController(_ controller: MFMailComposeViewController, didFinishWith result: MFMailComposeResult, error: Error?) {
         controller.dismiss(animated: true, completion: nil)
     }
